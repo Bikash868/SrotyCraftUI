afdd16c45bea56ef5ad3aaa6194078c014d9b241
Bikash Das <ursbikash868@gmail.com>
2024-07-30 00:44:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Board from '../src/views/Board'
+
+function App() {
+
+  return (
+      <div style={{height:'95vh', width:'98vw'}}>
+        <Board/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/views/Board/index.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const Board = () => {
+  return (
+    <div style={{background:'#dadada', height:'100%', width:'100%', overflow:'auto'}}>Board</div>
+  )
+}
+
+export default Board
